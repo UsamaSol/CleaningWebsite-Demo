8c286db647ce5cfa311f7d744c34b09e22721811
UsamaSol <usama.qamar121@gmail.com>
2025-06-22 12:51:53 -0700

    feat: enable system theme detection

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={roboto.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="light"
+          defaultTheme="system"
           enableSystem
           disableTransitionOnChange={false}
         >
